5e5d02176210b512e89f66d4b1c1089652996319
arshuaib <37807946+arshuaib@users.noreply.github.com>
2026-09-18 21:51:33 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const VERSION='flowtrack-cache-v3.1.0';
+const APP_SHELL=['./','./index.html'];
+
+self.addEventListener('install',event=>{
+ event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(APP_SHELL)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+ event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('flowtrack-cache-')&&k!==VERSION).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET') return;
+ const url=new URL(event.request.url);
+ if(url.origin!==self.location.origin) return;
+ event.respondWith(
+   fetch(event.request,{cache:'no-store'}).then(response=>{
+     const copy=response.clone();
+     caches.open(VERSION).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+     return response;
+   }).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html')))
+ );
+});
