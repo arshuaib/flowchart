a9b66fc5733f678a134f56395a5294af5398709c
arshuaib <37807946+arshuaib@users.noreply.github.com>
2026-09-22 06:36:18 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='flowtrack-network-first-v3.5.0';
+const CACHE='flowtrack-network-first-v3.5.1';
 self.addEventListener('install',event=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{
